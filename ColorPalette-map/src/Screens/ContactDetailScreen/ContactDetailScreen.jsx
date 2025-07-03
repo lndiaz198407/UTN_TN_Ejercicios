bc72031449a1c2a575f42895e2274500ca157139
LuisND <rtvdiaz.antartida@gmail.com>
2025-07-03 09:22:54 -0300

    Parcial

--- ColorPalette-map/src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
+++ ColorPalette-map/src/Screens/ContactDetailScreen/ContactDetailScreen.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import ContactContext from '../../Context/ContactContext'
+import { useParams } from 'react-router'
 
 export default function ContactDetailScreen() {
+  const { contact_id } = useParams()
+  console.log(contact_id)
+  const { contacts } = useContext(ContactContext)
+const userById = contacts.find(contact => contact.id === 2);
+  console.log('Usuario por ID:', userById);
+
+
   return (
     <div>
-            <div></div>
+            <div>
+
+            </div>
     </div>
   )
 }
